64249df1ac2504982d6e3b63998ec4b645ea6dfa
Lucia <learningleadtorebirth@gmail.com>
2017-10-10 22:47:39 +0800

    use context to simplify code

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -57,35 +57,40 @@ RSpec.describe CoursesController do
   end
 
   describe "POST create" do
-    it "doesn't create a new course record when course doesn't have a title" do
+    context "when course doesn't have a title" do
+      it "doesn't create a new course record" do
 
-      expect do
+        expect do
+          post :create, params: {:course => {:descriptoin => "bar"}}
+        end.to change{ Course.count }.by(0)
+      end
+
+      it "render new template" do
+
         post :create, params: {:course => {:descriptoin => "bar"}}
-      end.to change{ Course.count }.by(0)
+
+        expect(response).to render_template("new")
+      end
     end
 
-    it "render new template when course doesn't have title" do
+    context "when course has a title" do
+      it "create a new course record when course has a title" do
+        course = build(:course)
 
-      post :create, params: {:course => {:descriptoin => "bar"}}
+        expect do
+          post :create, params: {:course => attributes_for(:course)}
+        end.to change{ Course.count }.by(1)
+      end
 
-      expect(response).to render_template("new")
-    end
+      it "redirects to courses_path when course has title" do
+        course = build(:course)
 
-    it "create a new course record when course has a title" do
-      course = build(:course)
-
-      expect do
         post :create, params: {:course => attributes_for(:course)}
-      end.to change{ Course.count }.by(1)
+
+        expect(response).to redirect_to courses_path
+      end
     end
 
-    it "redirects to courses_path when course has title" do
-      course = build(:course)
-
-      post :create, params: {:course => attributes_for(:course)}
-
-      expect(response).to redirect_to courses_path
-    end
   end
 
 end
