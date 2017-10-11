c7d2b133617f4f36dfa549ae0770f0607978b5e3
Lucia <learningleadtorebirth@gmail.com>
2017-10-11 19:18:53 +0800

    refactor user sign in spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Home" do
   scenario "has navbar element" do
-    visit root_url
+    home_page.go
     expect(page).to have_css "nav.navbar"
   end
 
   scenario "welcomes user" do
-    visit root_url
+    home_page.go
     expect(page).to have_text "welcome"
+  end
+
+  def home_page
+    PageObjects::Pages::Home.new
   end
 end
---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "user sign in", :type => :feature do
+  before :each do
+    User.create(:email => "user@example.com", :password => "password")
+  end
+
+  it "sign_in the user" do
+    new_session_page.sign_in "user@example.com", "password"
+    expect(page).to have_content "user@example.com"
+  end
+end
+
+private
+
+def new_session_page
+  home_page.go
+  navbar.sign_in
+end
+
+def home_page
+  PageObjects::Pages::Home.new
+end
+
+def navbar
+  PageObjects::Application::Navbar.new
+end
