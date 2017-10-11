743dca51d7c35597f929e67028f790b596bf40ec
Lucia <learningleadtorebirth@gmail.com>
2017-10-11 18:37:34 +0800

    logout test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -11,4 +11,19 @@ RSpec.feature "User authentication" do
     click_button "Log in"
     expect(page).to have_text "user@example.com"
   end
+
+  scenario "user log out" do
+    create(:user, email: "user@example.com", password: "password")
+
+    visit "/users/sign_in"
+
+    within(".new_user") do
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+    end
+
+    click_button "Log in"
+    click_link "Logout"
+    expect(page).not_to have_text "user@example.com"
+  end
 end
